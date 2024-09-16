8e9d52d56ab92646c0a7d90b0f53f615772ae937
anassstya <408183@edu.itmo.ru>
2024-09-16 11:32:29 +0300

    changes

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,6 +1,21 @@
 import { Validator } from "../app";
 
-test('should check userName', ()=> {
-  let result = new Validator();
-  expect(result.validateUsername('ka-14-te')).toBe(true)
+test.each([
+    ['testName', true],
+    ['test11Name', true],
+    ['test-Name', true],
+    ['test_Name', true],
+    ['test777Name', true],
+    ['test7007Name', false],
+    ['test70707Name', false],
+    ['1testName1', false],
+    ['TESTNAME', true],
+    ['___testname___', false],
+    ['t1e2s3t4N5a6m7e', true],
+    ['234234234wewer-----', false],
+    ['234234234wewer-----', false],
+    ['234234234wewer-----', false],
+])('check nameValidity', (name, result) => {
+  let a = new Validator();
+  expect(a.validateUsername(name)).toBe(result)
 })
--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 export class Validator {
   validateUsername(name){
-    return /^[a-z]+[-_]*\d{0,3}[-_]*[a-z]+$/.test(name) ;
+    return /^[a-z]+[-_]*\d{0,3}[-_]*[a-z]+$/i.test(name);
   }
 }
 const a = new Validator();
-console.log(a.validateUsername('gd45-fdf'))
+console.log(a.validateUsername('test7007Name'))
